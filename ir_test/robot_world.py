aa261ae2f3cf5f77f1c9598a7b8c211af22a7dcf
Han <744475118@qq.com>
2021-10-25 20:04:22 +0800

    test

--- ir_test/robot_world.py
+++ ir_test/robot_world.py
@@ -1,7 +1,17 @@
 from ir_env import env_base
 
 world_name = 'robot_world.yaml'
-env = env_base(world_name = world_name, plot=True)
+env = env_base(world_name = world_name, plot=True, init_mode=0, robot_mode='diff')
+
+for i in range(300):
+
+    for robot in env.components['robots'].robot_list:
+        des_vel = robot.cal_des_vel()
+        robot.move_forward(des_vel)
+
+    env.render()
+
+
 
 
 
