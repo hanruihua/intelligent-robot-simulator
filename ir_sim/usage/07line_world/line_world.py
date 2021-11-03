aa3c231b045afabb41fce1a766abd513129b3c36
Han <744475118@qq.com>
2021-11-03 11:36:36 +0800

    add line collision

---
+++ ir_sim/usage/07line_world/line_world.py
@@ -0,0 +1,20 @@
+from ir_sim.env import env_base
+# from ir_sim.world import 
+
+world_name = 'line_world.yaml'
+env = env_base(world_name = world_name, plot=True, init_mode=0, robot_mode='diff')
+
+for i in range(300):
+
+    for robot in env.components['robots'].robot_list:
+        des_vel = robot.cal_des_vel()
+        robot.move_forward(des_vel)
+
+    env.render()
+    
+    if env.collision_check():
+        break
+
+env.show()
+
+
