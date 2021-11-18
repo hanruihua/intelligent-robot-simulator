29c4b607aba7fae43175b7503e7828cbc9d705ab
Han <744475118@qq.com>
2021-11-18 20:06:21 +0800

    add teleop keyboard

--- ir_sim/env/env_base.py
+++ ir_sim/env/env_base.py
@@ -71,7 +71,9 @@ class env_base:
             print('start to keyboard control')
             print('w: forward', 's: backforward', 'a: turn left', 'd: turn right', 
                   'q: decrease linear velocity', 'e: increase linear velocity',
-                  'z: decrease angular velocity', 'c: increase angular velocity')
+                  'z: decrease angular velocity', 'c: increase angular velocity',
+                  'alt+num: change current control robot id')
+                  
 
             listener = keyboard.Listener(on_press=self.on_press, on_release=self.on_release)
             listener.start()
