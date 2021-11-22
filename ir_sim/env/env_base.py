f693529385872135a55e1f701ae052d01403737c
Han <744475118@qq.com>
2021-11-22 13:07:16 +0800

    modify env time

--- ir_sim/env/env_base.py
+++ ir_sim/env/env_base.py
@@ -24,7 +24,7 @@ class env_base:
                 world_args = com_list['world']
                 self.__height = world_args.get('world_height', 10)
                 self.__width = world_args.get('world_width', 10)
-                self.__step_time = world_args.get('step_time', 0.1)
+                self.step_time = world_args.get('step_time', 0.1)
                 self.world_map =  world_args.get('world_map', None)
                 self.xy_reso = world_args.get('xy_resolution', 1)
                 self.yaw_reso = world_args.get('yaw_resolution', 5)
@@ -45,7 +45,7 @@ class env_base:
         else:
             self.__height = kwargs.get('world_height', 10)
             self.__width = kwargs.get('world_width', 10)
-            self.__step_time = kwargs.get('step_time', 0.1)
+            self.step_time = kwargs.get('step_time', 0.1)
             self.world_map = kwargs.get('world_map', None)
             self.xy_reso = kwargs.get('xy_resolution', 1)
             self.yaw_reso = kwargs.get('yaw_resolution', 5)
@@ -100,11 +100,11 @@ class env_base:
         self.components['map_matrix'] = self.map_matrix
         self.components['xy_reso'] = self.xy_reso
 
-        self.components['robots'] = env_robot(robot_class=robot_class, robot_num=self.robot_number, step_time=self.__step_time, **{**self.robots_args, **kwargs})
+        self.components['robots'] = env_robot(robot_class=robot_class, robot_num=self.robot_number, step_time=self.step_time, **{**self.robots_args, **kwargs})
 
-        self.components['cars'] = env_car(car_class=car_class, car_num=self.car_number, step_time=self.__step_time, **{**self.cars_args, **kwargs})
+        self.components['cars'] = env_car(car_class=car_class, car_num=self.car_number, step_time=self.step_time, **{**self.cars_args, **kwargs})
 
-        self.components['obs_cirs'] = env_obs_cir(obs_cir_class=obs_cir_class, obs_cir_num=self.obs_cir_number, step_time=self.__step_time, **{**self.obs_cirs_args, **kwargs})
+        self.components['obs_cirs'] = env_obs_cir(obs_cir_class=obs_cir_class, obs_cir_num=self.obs_cir_number, step_time=self.step_time, **{**self.obs_cirs_args, **kwargs})
 
         self.components['obs_lines'] = env_obs_line(**{**self.obs_lines_args, **kwargs})
         
