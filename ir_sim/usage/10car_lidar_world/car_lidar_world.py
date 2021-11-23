d0db342e474341b7f7d9069638e8c584f4d21207
Han <744475118@qq.com>
2021-11-23 10:33:41 +0800

    add assemblage

--- ir_sim/usage/10car_lidar_world/car_lidar_world.py
+++ ir_sim/usage/10car_lidar_world/car_lidar_world.py
@@ -7,7 +7,6 @@ env = env_base(world_name = world_name, plot=True, init_mode=0, robot_mode='diff
 for i in range(300):
 
     des_vel = env.car.cal_des_vel()
-
     env.car_step(des_vel)
     env.render()
 
---
+++ ir_sim/usage/13assemblage/assemblage.py
@@ -0,0 +1,26 @@
+from ir_sim.env import env_base
+from pathlib import Path
+
+world_name = 'assemblage.yaml'
+env = env_base(world_name = world_name, plot=True, init_mode=0, robot_mode='diff', teleop_key=True)
+
+image_path = Path(__file__).parent / 'image'
+gif_path = Path(__file__).parent / 'gif'
+
+for i in range(300):
+
+    # des_vel = env.car.cal_des_vel()
+
+    env.car_step(env.key_vel)
+    # env.robot_step(env.key_vel)
+    env.render()
+
+    env.save_fig(image_path, i) 
+
+    if env.collision_check() or env.arrive_check():
+        break
+
+env.save_ani(image_path, gif_path)
+env.show()
+
+
